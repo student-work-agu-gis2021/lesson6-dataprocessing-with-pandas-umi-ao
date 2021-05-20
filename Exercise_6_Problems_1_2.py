0f32406d3fc07aae68a312c6f6e185c044226816
umiao <>
2021-05-20 09:27:59 +0000

    Find the date of the most recent (last) observation

--- Exercise_6_Problems_1_2.py
+++ Exercise_6_Problems_1_2.py
@@ -80,7 +80,7 @@ print('Date of the first observation:',first_obs)
 last_obs = None
 
 # YOUR CODE HERE 6
-
+first_obs = data['DATE'].max
 #CAUTION!!! DON'T EDIT THIS PART START
 # Print out the solution:
 print('Date of the last observation:', last_obs)
