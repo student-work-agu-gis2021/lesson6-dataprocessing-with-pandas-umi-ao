9a7034029de5bbdebe72ad842685c8055d35c9d6
umiao <>
2021-05-22 18:55:50 +0000

    recorrect day_count

--- Exercise_6_Problems_1_2.py
+++ Exercise_6_Problems_1_2.py
@@ -60,7 +60,7 @@ print('Number of no-data values in column "TMIN":', tmin_nodata_count)
 
 day_count = None 
 #YOUR CODE HERE 4
-day_count = data['DATE'].len()
+day_count = len(data['DATE'])
 #CAUTION!!! DON'T EDIT THIS PART START
 # Print out the solution:
 print("Number of days:", day_count)
